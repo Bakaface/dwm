875de49b67ec68fb776577ba4f5f2ff45e325e9c
Albert <aface@aface.pro>
2020-07-24 21:39:30 +0500

    Revert "Apply focusurgent"
    This reverts commit 53788eb2240d9a445c751842fac897ccdec4a15e.

--- config.h
+++ config.h
@@ -70,7 +70,6 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
-#include "focusurgent.c"
 #include "movestack.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -113,7 +112,6 @@ static Key keys[] = {
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_Escape, quit,           {0} },
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
-	{ MODKEY,                       XK_u,      focusurgent,    {0} },
 };
 
 /* button definitions */
